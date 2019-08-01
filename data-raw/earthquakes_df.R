f7c5bf9772f6f47fb9b2212f9fc331a09c749d3e
Erick Cuevas <erickcuevasf@users.noreply.github.com>
2019-08-01 16:56:02 -0500

    Change in format

--- data-raw/earthquakes_df.R
+++ data-raw/earthquakes_df.R
@@ -3,13 +3,28 @@
 # usethis::use_data(earthquakes_df)
 
 
-earthquakes_df <- read.csv("data-raw/SSNMX_catalogo_19000801_20190801.csv", skip = 4)
+earthquakes_df <- read_csv("data-raw/SSNMX_catalogo_19000801_20190801.csv", skip = 4)
 earthquakes_df <- na.omit(earthquakes_df)
 
 earthquakes_df <- earthquakes_df[, -c(8:10)]
 earthquakes_names <- c("date", "hour", "magnitude", "latitude",
                     "longitude","depth", "location")
 colnames(earthquakes_df) <- earthquakes_names
+earthquakes_df$magnitude <- as.numeric(earthquakes_df$magnitude)
+
+
+earthquakes_df_states <- data.frame(earthquakes_df$location)
+states <- data.frame(do.call('rbind',
+                strsplit(as.character(earthquakes_df_states$earthquakes_df.location),
+                ',',fixed=TRUE)))
+colnames(states) <- c("location","state")
+
+earthquakes_df <- earthquakes_df[,-7]
+earthquakes_df <- cbind(earthquakes_df, states)
+
+
+
+# earthquakes_df_states<-read.delim(earthquakes_df_states)
 
 
 save(earthquakes_df, file = "data/earthquakes_df.rda")

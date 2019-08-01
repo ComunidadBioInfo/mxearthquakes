fe44d1432157189d7138adc056e83ea62e4bac31
Erick Cuevas <erickcuevasf@users.noreply.github.com>
2019-08-01 13:58:54 -0500

    Generate of data directory

--- data-raw/earthquakes_df.R
+++ data-raw/earthquakes_df.R
@@ -1,6 +1,6 @@
 ## code to prepare `earthquakes_df` dataset goes here
 
-# usethis::use_data("earthquakes_df")
+# usethis::use_data(earthquakes_df)
 
 
 earthquakes_df <- read.csv("data-raw/SSNMX_catalogo_19000801_20190801.csv", skip = 4)
@@ -11,3 +11,4 @@ earthquakes_names <- c("date", "hour", "magnitude", "latitude",
                     "longitude","depth", "location")
 colnames(earthquakes_df) <- earthquakes_names
 
+save(earthquakes_df, file = "data/earthquakes.rda")

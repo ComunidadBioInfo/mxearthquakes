9a27ba4524b08509c5a94b18bb85251476e3de04
Erick Cuevas <erickcuevasf@users.noreply.github.com>
2019-08-01 13:42:39 -0500

    "Changes of data_raw to omit 4 first lines and 7 last lines"

--- data-raw/earthquakes_df.R
+++ data-raw/earthquakes_df.R
@@ -1,6 +1,12 @@
 ## code to prepare `earthquakes_df` dataset goes here
 
-usethis::use_data("earthquakes_df")
+# usethis::use_data("earthquakes_df")
 
 
-earthquakes_df<-readLines("../")
+earthquakes_df<-read.csv("data-raw/SSNMX_catalogo_19000801_20190801.csv", skip = 4)
+earthquakes_df<-na.omit(earthquakes_df)
+# length_earth<-nrow(earthquakes_df)
+# earthquakes_table<-data.frame(earthquakes_df[c(-1:-4, -length_earth-7:-length_earth),])
+# earthquakes_df<-read.csv(earthquakes_table)
+
+

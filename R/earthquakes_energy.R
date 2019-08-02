6da32323640b0ce78ac1c934ee844629225be152
Erick Cuevas <erickcuevasf@users.noreply.github.com>
2019-08-02 10:28:31 -0500

    Change arg to erg

--- R/earthquakes_energy.R
+++ R/earthquakes_energy.R
@@ -3,7 +3,7 @@
 #' This function calculate the energy of a magnitude earthquake
 #'
 #' @param x A magnitude of earhquake in Richter magnitude scale
-#' @param y A [character] to select units of energy: "arg" OR "joule"
+#' @param y A [character] to select units of energy: "erg" OR "joule"
 #'
 #' @return
 #' A number of the energy in the units

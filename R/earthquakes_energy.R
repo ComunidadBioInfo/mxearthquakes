f19cd9e4e0965193689720886a8940fe7eaa8904
Erick Cuevas <erickcuevasf@users.noreply.github.com>
2019-08-02 10:08:51 -0500

    Documentation of earthquake_energy function

--- R/earthquakes_energy.R
+++ R/earthquakes_energy.R
@@ -1,4 +1,16 @@
-
+#' **Earthquakes energy calculation**
+#'
+#' _This function calculate the energy of a magnitude earthquake_
+#'
+#' @param x A magnitude of earhquake in Richter magnitude scale
+#' @param y A [character] to select units of energy: "arg" OR "joule"
+#'
+#' @return
+#' A number of the energy
+#' @export
+#'
+#' @examples
+#' a<-earthquakes_energy(6, "erg")
 
 
 earthquakes_energy <- function(x, y = "erg") {
@@ -19,6 +31,5 @@ earthquakes_energy <- function(x, y = "erg") {
 }
 
 
-# ejemplo
-# a<-earthquakes_energy(6, "erg")
+
 
